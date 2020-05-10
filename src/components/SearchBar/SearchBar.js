9f948147e82f24b7def78fa2f936ed4a399e1e80
SergeySolod <solodsergey287@gmail.com>
2020-05-10 22:30:32 +0300

    Add Redux-form in SearchBarForm

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import {Paper, TextField} from '@material-ui/core'
+import {Field, reduxForm} from "redux-form";
 
-const SearchBar = () => {
+const SearchBarForm = (props) => {
     return (
         <h1>
             <Paper elevation={6} style={{padding: '25px'}}>
-                <form>
+                <form onSubmit={props.handleSubmit}>
                     <TextField fullWidth label='Search...' onChange/>
                 </form>
             </Paper>
         </h1>
+    )
+}
+
+const SearchBarReduxForm = reduxForm({form: 'Search'})(SearchBarForm)
+
+
+const SearchBar = (props) => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+    return (
+        <Paper elevation={6} style={{padding: '25px'}}>
+            <SearchBarReduxForm onSubmit={onSubmit}/>
+        </Paper>
     )
 }
 
