1969766d766b8f0a4a17de8269f91c55e7b260fc
SergeySolod <solodsergey287@gmail.com>
2020-05-10 21:45:50 +0300

    Add grid from material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Grid} from '@material-ui/core'
+import SearchBar from "./components/SearchBar/SearchBar";
+import VideoDetail from "./components/VideoDetail/VideoDetail";
+import VideoList from "./components/VideoList/VideoList";
 
 function App() {
   return (
-    <div>
-     Hello, YouTube clone
-    </div>
+   <Grid justify='center' container spacing={16}>
+       <Grid item sx={12}>
+           <Grid container spacing={16}>
+                <Grid item xs={12}>
+                    <SearchBar/>
+                </Grid>
+               <Grid item xs={8}>
+                   <VideoDetail/>
+               </Grid>
+               <Grid item xs={4}>
+                   <VideoList/>
+               </Grid>
+           </Grid>
+       </Grid>
+   </Grid>
   );
 }
 
