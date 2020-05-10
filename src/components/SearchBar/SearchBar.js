31af2b110bc07f70367d9a5ed9c319d7af97e769
SergeySolod <solodsergey287@gmail.com>
2020-05-10 22:24:40 +0300

    SearchBar and add Redux-form

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {Paper, TextField} from '@material-ui/core'
 
 const SearchBar = () => {
     return (
         <h1>
-            This is a Search Bar component
+            <Paper elevation={6} style={{padding: '25px'}}>
+                <form>
+                    <TextField fullWidth label='Search...' onChange/>
+                </form>
+            </Paper>
         </h1>
     )
 }
